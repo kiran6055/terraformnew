725d378a1944d706409f7ae2b7568d23b0da616e
kiran6055 <kirandevopskumar@gmail.com>
2023-09-16 16:59:54 +0530

    kk

---
+++ state/vpc.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc-automated"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "publicsubnet"
+  }
+}
+
+
+
