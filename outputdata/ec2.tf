da907e43e4252a9d2ac6f6c3c38832d75a95f37d
kiran6055 <kirandevopskumar@gmail.com>
2023-09-17 10:06:59 +0530

    kk

--- outputdata/ec2.tf
+++ outputdata/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "kiran"{
 
-    ami =  ami-05552d2dcf89c9b2#AMI ID will be different every region, this is OS
+    ami =  "ami-05552d2dcf89c9b2" #AMI ID will be different every region, this is OS
     instance_type = "t3.micro"
 }
