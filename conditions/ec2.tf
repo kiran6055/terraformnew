dc732c743951185ecc562e9535fed67473492f5b
kiran6055 <kirandevopskumar@gmail.com>
2023-09-19 15:30:37 +0530

    kk

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -8,7 +8,7 @@
  
 # }
 
-
+# this is condition expression here we have given  condition is true then it will take t3.micro if condition is false then it will take t3.medium we have given bool variable in variables.tf variable name a type
 resource "aws_instance" "webnew" {
    ami           = "ami-0ff30663ed13c2290"
    instance_type = var.type ? "t3.micro" : "t3.medium"
