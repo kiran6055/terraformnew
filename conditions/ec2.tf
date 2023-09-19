cdb0138437ca3cc248d0eeb69dccec56989ed240
kiran6055 <kirandevopskumar@gmail.com>
2023-09-19 15:25:56 +0530

    kk

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,9 +1,16 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0ff30663ed13c2290"
-  instance_type = "t2.micro"
-  count         = length(var.tags)
-  tags = {
-    Name = var.tags[count.index]
-  }
+# resource "aws_instance" "web" {
+#   ami           = "ami-0ff30663ed13c2290"
+#   instance_type = "t2.micro"
+#   count         = length(var.tags)
+#   tags = {
+#     Name = var.tags[count.index]
+#   }
  
+# }
+
+
+resource "aws_instance" "web" {
+   ami           = "ami-0ff30663ed13c2290"
+   instance_type = var.type ? "t3.micro" : "t3.medium"
+
 }
--- conditions/variables.tf
+++ conditions/variables.tf
@@ -2,3 +2,8 @@ variable "tags" {
   type = list
   default = ["webserver","app-server","db-server"]
 }
+
+variable "type" {
+  type = bool
+  default = true
+}
