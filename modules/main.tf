66cb22857394e9b526a54b1c8ae1fcfe317a7213
kiran6055 <kirandevopskumar@gmail.com>
2023-09-19 19:39:12 +0530

    kk

--- modules/main.tf
+++ modules/main.tf
@@ -4,3 +4,30 @@ module "vpc" {
   tags = var.tags
 }
 
+
+resource "aws_security_group" "web" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
